2f8b1924a5981394cbe4261d31db254241fd810e
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 00:37:51 +0100

    probando un objeto Persona

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona persona = new Persona();
+        persona.nombre = "Pepe Pérez";
+        persona.edad = 31;
+        persona.fechaContratacion = new Date(90, Calendar.FEBRUARY, 1);
+
+        System.out.println("Se ha registrado a " + persona.nombre + " (" + persona.edad + " años; contratado en 19" + persona.fechaContratacion.getYear() + ")");
     }
 }
---
+++ src/Persona.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Persona {
+    String nombre;
+    int edad;
+    Date fechaContratacion;
+
+}
