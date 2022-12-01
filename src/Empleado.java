670f6798736ff0939df04a9b6e87c3eb3a119d5e
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 04:24:34 +0100

    migrado a LocalDate

--- src/Empleado.java
+++ src/Empleado.java
@@ -3,9 +3,9 @@ import java.util.Date;
 class Empleado {
     private final String nombre;
     private int edad;
-    private final Date fechaContratacion;
+    private final LocalDate fechaContratacion;
 
-    private Empleado(String nombre, int edad, Date fechaContratacion) {
+    private Empleado(String nombre, int edad, LocalDate fechaContratacion) {
             this.nombre = nombre;
             this.edad = edad;
             this.fechaContratacion = fechaContratacion;
@@ -24,7 +24,7 @@ class Empleado {
         return edad;
     }
 
-    public Date getFechaContratacion() {
+    public LocalDate getFechaContratacion() {
         return fechaContratacion;
     }
 
@@ -37,7 +37,7 @@ class Empleado {
          * Método factoría que permite comprobar si la edad es válida antes de llamar al constructor.
          * El constructor es ahora privado, impidiendo llamarlo directamente desde otra clase.
          */
-        public static Empleado crearEmpleado(String nombre, int edad, Date fechaContratacion) {
+        public static Empleado crearEmpleado(String nombre, int edad, LocalDate fechaContratacion) {
             if (edad < 0) throw new RuntimeException("Un empleado no puede tener edad negativa");
             else return new Empleado(nombre, edad, fechaContratacion);
         }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.Calendar;
-import java.util.Date;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +7,7 @@ public class Main {
         //Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         //p.edad = -31; // Arreglado: error de compilación. edad no es accesible
 
-        Empleado p = Empleado.crearEmpleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
+        Empleado p = Empleado.crearEmpleado("Pepe Pérez", 31, LocalDate.of(2020, 1, 8));
         System.out.println(p);
 
         //p.nombre = "Manolo";
@@ -24,13 +24,9 @@ public class Main {
         System.out.println("Si quiero mostrar solo el nombre: " + p.getNombre());
         System.out.println("Si quiero mostrar solo la edad: " + p.getEdad());
 
-        /* Aunque la variable fechaContratación es inmutable (final), su tipo (Date) es mutable, de modo que
-        todavía podría modificarse así: */
-        p.getFechaContratacion().setYear(10);
-
-        /* Además, Date es una clase de la primera versión de Java que solo almacena dos dígitos para años, de modo
-        que al ponerle el año 10 pensando en 2010, nos encontraremos que no sabemos si se refiere a 1910. */
-        System.out.println("Nueva fecha de contratación: " + p.getFechaContratacion().getYear());
+        /* Para el manejo de fechas, se debe utilizar LocalDate en lugar de Date.
+        LocalDate es inmutable y almacena años completos, además de otras muchas mejoras.  */
+        System.out.println("Año de contratación: " + p.getFechaContratacion().getYear());
 
     }
 }
