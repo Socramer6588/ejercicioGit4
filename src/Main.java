1f7215554a97731e5e60fb876ba46916826b4520
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 03:35:52 +0100

    resuelto

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
-        System.out.println(p);
-
+        //Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         //p.edad = -31; // Arreglado: error de compilación. edad no es accesible
 
-        Empleado p2 = new Empleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
+
+        Empleado p = Empleado.crearEmpleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
 
+        /*
+        Ya no se puede pasar una edad negativa: lanzaría una excepción en tiempo de ejecución.
         
+        Empleado p2 = Empleado.crearEmpleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
+        System.out.println(p2);
+         */
+
+
     }
 }
