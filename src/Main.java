61af336bfb831f2c6d93eaf2d8e99f46209ca6f4
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 03:07:36 +0100

    diseñamos las pruebas de esta funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
+        Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
+
+
+        p.nombre = "Manolo"; // Cambio de nombre. No se debe permitir
+        p.fechaContratacion = new Date(99, Calendar.FEBRUARY, 1); // Cambio de fecha. No se debe permitir
     }
 }
