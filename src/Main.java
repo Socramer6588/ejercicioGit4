67f3ced268d9c2c92d1cdf6d188f20de565b5c6f
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 04:17:05 +0100

    mostrando los problemas de Date

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,13 @@ public class Main {
         System.out.println("Si quiero mostrar solo el nombre: " + p.getNombre());
         System.out.println("Si quiero mostrar solo la edad: " + p.getEdad());
 
+        /* Aunque la variable fechaContratación es inmutable (final), su tipo (Date) es mutable, de modo que
+        todavía podría modificarse así: */
+        p.getFechaContratacion().setYear(10);
+
+        /* Además, Date es una clase de la primera versión de Java que solo almacena dos dígitos para años, de modo
+        que al ponerle el año 10 pensando en 2010, nos encontraremos que no sabemos si se refiere a 1910. */
+        System.out.println("Nueva fecha de contratación: " + p.getFechaContratacion().getYear());
+
     }
 }
