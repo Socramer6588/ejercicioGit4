948bd2fdb384c9d65cf503ebb32981843adbce9f
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 01:39:22 +0100

    probando objeto empleado

---
+++ src/Empleado.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+class Empleado {
+    final String nombre;
+    Date birthDate;
+
+    Empleado(String name, Date birthDate) {
+        this.nombre = name;
+        this.birthDate = birthDate;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
+        System.out.println("Empleado: " + p.nombre + " (nacido en 19" + p.birthDate.getYear() + ")");
     }
 }
