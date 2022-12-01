d2797029889eed896598b36dedf695b0f4b2f2ca
Marcos Domínguez Ordóñez <marcos6588@gmail.com>
2022-12-01 01:42:24 +0100

    encapsulando toString de Empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,12 +1,15 @@
 import java.util.Date;
-
 class Empleado {
     final String nombre;
     Date birthDate;
-
     Empleado(String name, Date birthDate) {
         this.nombre = name;
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
-        System.out.println("Empleado: " + p.nombre + " (nacido en 19" + p.birthDate.getYear() + ")");
+        System.out.println(p);
     }
 }
